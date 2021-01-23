7125912be7b31d0a84535e0506a8b24ffb270934
Daniel Martinez <xdanielmtz@gmail.com>
2021-01-23 17:07:38 -0500

    styled table

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Search = () => {
-  return (
-    <div className="md:flex justify-between my-6">
-      <div className="md:mb-0 mb-4"></div>
-      <form>
-        <input
-          className="shadow pl-5 rounded w-full py-1"
-          type="text"
-          placeholder="search employee"
-        />
-      </form>
-    </div>
-  );
-};
+class Search extends Component {
+  render() {
+    return (
+      <div>
+        <br />
+        <div className="text-center">
+          <form>
+            <input
+              value=""
+              type="text"
+              placeholder="Search Employee"
+            />
+          </form>
+        </div>
+        <br />
+      </div>
+    );
+  }
+}
 
 export default Search;
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,38 +2,47 @@ import React from "react";
 
 const Table = () => {
   return (
-    <div>
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <div>
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Image</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">DOB</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
