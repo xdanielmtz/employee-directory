8f2bfe35da39fa2e6244e6b64d931513b9926e50
Daniel Martinez <xdanielmtz@gmail.com>
2021-01-25 14:27:43 -0500

    data is now displayed on the screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Banner from "./components/Banner/Banner";
 import Table from "./components/Table/Table";
-import Search from "./components/Search/Search";
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <Search />
       <Table />
     </div>
   );
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,49 +1,71 @@
-import React from "react";
+import axios from "axios";
+import React, { Component } from "react";
 
-const Table = () => {
-  return (
-    <>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
-            <div>
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">Image</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Phone</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">DOB</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                  </tr>
-                </tbody>
-              </table>
+class Table extends Component {
+  state = {
+    results: [],
+  };
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    axios
+      .get("https://randomuser.me/api/?results=50")
+      .then((response) => this.setState({ results: response.data.results }))
+      .then((res) => console.log(this.state.results));
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          <br />
+          <div className="text-center">
+            <form>
+              <input value="" type="text" placeholder="Search Employee" />
+            </form>
+          </div>
+          <br />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div>
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Image</th>
+                      <th scope="col">Name</th>
+                      <th scope="col">Phone</th>
+                      <th scope="col">Email</th>
+                      <th scope="col">Age</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.state.results.map((element) => (
+                      <tr>
+                        <th scope="row">
+                          <img src={element.picture.thumbnail}></img>
+                        </th>
+                        <td>
+                          {element.name.first} {element.name.last}
+                        </td>
+                        <td>{element.cell}</td>
+                        <td>{element.email}</td>
+                        <td>{element.dob.age}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  }
+}
 
 export default Table;
